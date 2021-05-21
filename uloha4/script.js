2eb6d6cab27671821dc7983c8d586ff0e931873f
Tinka8 <lenmatka818@gmail.com>
2021-05-21 14:14:04 +0200

    main slide bar

--- uloha4/script.js
+++ uloha4/script.js
@@ -142,3 +142,15 @@ document.body.onscroll = function() {
     	arrow.classList.remove('show');
     }
 }
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
